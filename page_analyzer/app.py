c80643aa7f4d2d6ac1dc2d046ff2cc6ad50cf245
NevermoreKatana <grenkachannel@hotmail.com>
2023-09-15 21:07:57 +0300

    add new checkers and routes

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, flash, redirect
 from page_analyzer.validator import validate
 import os
+from page_analyzer.database import add_new_url, take_url_id, take_url_info, take_all_entity
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'VANYA LOVE'
@@ -16,7 +18,19 @@ def index():
 def urls():
     url = request.form.get('url')
     errors = validate(url)
-    if len(errors) > 0:
-        flash('asdas')
+    if errors:
         return render_template('index.html', errors=errors, url=url), 422
-    return f'tut budet analiz {url}'
+    add_new_url(url)
+    id = take_url_id(url)
+    return redirect(f'/urls/{id}')
+
+@app.route('/urls/<id>')
+def show_one_url(id):
+    info_about_url = take_url_info(id)
+    return render_template('/urls/one.html', url = info_about_url)
+
+
+@app.route('/urls', methods=['GET'])
+def show_urls():
+    entities = take_all_entity()
+    return render_template('/urls/all_urls.html', entities = entities)
