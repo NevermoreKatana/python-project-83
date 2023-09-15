51f830367fb1ec64e79330c35495cc6da3fe450c
NevermoreKatana <grenkachannel@hotmail.com>
2023-09-15 22:00:46 +0300

    dev

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -10,10 +10,10 @@ db_params = {
 }
 
 conn = psycopg2.connect(**db_params)
-
+cursor = conn.cursor()
 
 def availability_check_url(url):
-    cursor = conn.cursor()
+
     cursor.execute(f"SELECT name FROM urls WHERE name = '{url}'")
     checked = cursor.fetchall()
     cursor.close()
@@ -24,7 +24,6 @@ def availability_check_url(url):
 
 def add_new_url(url):
     if availability_check_url(url):
-        cursor = conn.cursor()
         current_time = datetime.datetime.now()
         fromated_time = current_time.strftime('%Y-%m-%d')
         cursor.execute(f"INSERT INTO urls (name, created_at) VALUES ('{url}', '{fromated_time}')")
@@ -33,7 +32,6 @@ def add_new_url(url):
 
 
 def take_url_id(url):
-    cursor = conn.cursor()
     cursor.execute(f"SELECT id FROM urls WHERE name = '{url}'")
     id = cursor.fetchall()[0][0]
     cursor.close()
@@ -41,7 +39,6 @@ def take_url_id(url):
 
 
 def take_url_info(id):
-    cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM urls WHERE id = {id}")
     info = cursor.fetchall()
     cursor.close()
@@ -49,7 +46,6 @@ def take_url_info(id):
 
 
 def take_all_entity():
-    cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM urls")
     entities = cursor.fetchall()
     cursor.close()
