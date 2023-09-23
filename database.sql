637ff4c95ff3cf91b022cec73e8a5aaae5b9ae23
NevermoreKatana <grenkachannel@hotmail.com>
2023-09-23 14:44:13 +0300

    Добавлены обработчики

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
-create table urls(
+CREATE TABLE IF NOT EXISTS urls(
     id         bigint generated always as identity primary key,
     name       varchar(255) not null unique,
     created_at date not null
 );
-create table url_checks(
+CREATE TABLE IF NOT EXISTS url_checks(
     id          bigint generated always as identity primary key,
     url_id      bigint references urls,
     status_code integer,
