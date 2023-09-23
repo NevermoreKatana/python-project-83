72538371dbf65c43a3b55f5dda29760f8eb850e7
NevermoreKatana <grenkachannel@hotmail.com>
2023-09-23 14:43:34 +0300

    Больше нет бед с форматированием и убрана транзакция

--- database.sql
+++ database.sql
@@ -1,22 +1,14 @@
-BEGIN;
-create table urls
-(
-    id         bigint generated always as identity
-        primary key,
-    name       varchar(255) not null
-        unique,
-    created_at date         not null
+create table urls(
+    id         bigint generated always as identity primary key,
+    name       varchar(255) not null unique,
+    created_at date not null
 );
-create table url_checks
-(
-    id          bigint generated always as identity
-        primary key,
-    url_id      bigint
-        references urls,
+create table url_checks(
+    id          bigint generated always as identity primary key,
+    url_id      bigint references urls,
     status_code integer,
     h1          varchar(255),
     title       varchar(255),
     description varchar(255),
     created_at  date
 );
-END;
